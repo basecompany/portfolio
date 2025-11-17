e523600d2820251b74aedc4266c217f41c122eed
Ilyass Baba <ilyassbaba.me@gmail.com>
2025-11-17 21:59:45 +0100

    SYNC

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import {Routes, Route} from "react-router-dom";
+// import {Routes, Route} from "react-router-dom";
 import './App.css'
 import Portfolio from "./Pages/portfolio/Portfolio.tsx";
-export const App = () => (
-    <Routes>
-        <Route path="/portfolio" element={<Portfolio />} />
-    </Routes>
-);
+// export const App = () => (
+//     <Routes>
+//         <Route element={<Portfolio />} />
+//     </Routes>
+// );
 
+export const App = () => {
+    return <Portfolio/>;
+}
